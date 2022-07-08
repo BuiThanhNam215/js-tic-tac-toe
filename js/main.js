fae52c79d749aa8ced7f88490d776f99f161de20
Nam Bui <BuiThanhNam215>
2022-07-08 22:25:46 +0700

    add selecters

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 /**
  * Global variables
  */
+import {
+  getCellElementList,
+  getCurrentTurnElement,
+  getCellElementAtIdx,
+  getGameStatusElement,
+} from "./selectors.js";
+
+// console.log(getCellElementList());
 let currentTurn = "cross";
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
